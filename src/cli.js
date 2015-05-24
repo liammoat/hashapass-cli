01136d0fa94965d3110efdb819ef4d67ca2ec59d
liammoat <contact@liammoat.com>
2015-05-24 18:31:58 +0000

    feat(secure): add secure mode, for using two master passwords

--- src/cli.js
+++ src/cli.js
@@ -11,16 +11,34 @@ var bold = chalk.bold;
 program
   .version(package.version)
   .description(package.description)
-  .option('-l, --length <length>', 'Set the length of the generated result', 8);
+  .option('-l, --length <length>', 'Set the length of the generated result', 8)
+  .option('-s, --secure', 'Provide two master passwords for an extra secure result');
 
 program.parse(process.argv); 
 
-inquirer.prompt([
+var steps = [
   { name: 'parameter', type: 'type', message: 'Parameter' },
   { name: 'master', type: 'password', message: 'Master password' }
-], function (answers) {
-  var result = hashapass.generate(answers.parameter, answers.master, {
+];
+
+if (program.secure) {
+  steps.push({ name: 'master2', type: 'password', message: 'Second master password' });
+}
+
+function output(result) {
+  console.log(green('='), bold('Result:'), result);
+}
+
+inquirer.prompt(steps, function (answers) {
+  var result1 = hashapass.generate(answers.parameter, answers.master, {
     len: program.length
   });
-  console.log(green('='), bold('Result:'), result);
+  
+  if (!program.secure) return output(result1);
+  
+  var result2 = hashapass.generate(result1, answers.master2, {
+    len: program.length
+  });
+  
+  output(result2);
 });
